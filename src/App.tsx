07d44c76d94fb711f39578ae0789b38a1897f454
Marcus Pontes <marcusporama@gmail.com>
2025-04-24 21:18:27 -0300

    feat: included safeAreaProvider and change statusbar Color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import StackNavigation from './routes/StackNavigation';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StatusBar} from 'react-native';
 
 function App() {
   return (
     <NavigationContainer>
-      <StackNavigation />
+      <SafeAreaProvider>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+          animated
+          showHideTransition="slide"
+        />
+
+        <StackNavigation />
+      </SafeAreaProvider>
     </NavigationContainer>
   );
 }
